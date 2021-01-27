46cc5be64719d7aadeab0f88ab97274b21542a6e
Will Westwood <wwstwd@gmail.com>
2021-01-27 02:22:11 +0000

    Bascially done, just need a name and some hints. Maybe I'll call it "fSociety"

--- js/pathways.js
+++ js/pathways.js
@@ -47,6 +47,38 @@ const directoryMap = {
 }
 
 const commands = {
+    "help, ?": "Use: \"help\"; Opens this dialogue.",
+    "hint": "Use: \"hint\"; Displays a hint in the story dialogue box at the top.",
     "ls": "Use: \"ls\"; Lists files in current directory.",
-    "cd": "Use: \"cd [targetDirectory]\"; Changes Directory."
+    "cd": "Use: \"cd [targetDirectory]\"; Changes Directory.",
+    "open": "Use \"open [file]\"; Opens a file for you."
 }
+
+const files = ["fSociety.sh", "README.md", "passwords.txt"]
+
+const endings = [
+    {
+        "title": "Useless",
+        "description": "You took way too long and now the system has crashed!<br>You quit before you get fired and now you live alone on a private island with 20 cats."
+    },
+    {
+        "title": "You Got Scared",
+        "description": "The pressure got to you and so you ran away, now you live in an undisclosed loaction with a tinfoil hat on at all times."
+    },
+    {
+        "title": "Poser",
+        "description": "Stealing someones code is one thing, but pretending its yours is stupid, the original group hack you and now all you own is a small rock you've named dave."
+    },
+    {
+        "title": "Snitches Get Stitches",
+        "description": "The tech company and your employer love you but the world hates you, shame isn't it?"
+    },
+    {
+        "title": "Sherlock Wannabe",
+        "description": "You tried to investigate and got scared, even a cat could find out more information than you"
+    },
+    {
+        "title": "Vigilante",
+        "description": "The rootkit wiped all credit histories and debts across the whole world and now you are renowned as a hero ammoung the common folk, although the powerful are hunting you down."
+    }
+]
--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-const terminalInput = document.getElementById("terminalInput")
-const terminalOutputHolder = document.getElementById("terminalOutputHolder")
+const terminalInput = document.getElementById("terminalInput");
+const terminalOutputHolder = document.getElementById("terminalOutputHolder");
 
-var currentDir = "~/"
+var currentDir = "~/";
 var currentDirObject = directoryMap["~/"];
-var beforeString = "admin@kali " + currentDir + ": "
+var beforeString = "admin@kali " + currentDir + ": ";
+var defaultMode = true;
+var trustTest = false;
+var investigate = false;
+
+
+var commandHistory = [];
+var commandPosition;
 
 // document.querySelector(':root').style.setProperty('--directory', "admin@kali " + currentDir + ": ")
+
+window.setTimeout(function(){
+    gameOver(0);
+}, 300000)
 
 function terminalFocus() {
     terminalInput.focus();
@@ -16,18 +27,61 @@ terminalFocus();
 terminalInput.addEventListener('keydown', function(e){
     var command = terminalInput.innerHTML
     if(e.key == "ArrowLeft" || e.key == "ArrowRight" || e.key == "Tab"){
+    // if(e.key == "Tab"){
         e.preventDefault();
-    } else if (e.key == "Enter") {
+    } else if (e.key == "Enter" && defaultMode) {
         e.preventDefault();
-        
+
         input(command);
         
         if (command == "") {
             return
         }
 
+        commandHistory.push(command)
+        commandPosition = commandHistory.length;
+        // console.log(commandPosition)
         runCommand(command)
 
+    } else if (e.key == "Enter" && !defaultMode) {
+        e.preventDefault();
+
+        if (!investigate && !trustTest) {
+            rootkitOption(command);
+        } else if (investigate && !trustTest) {
+            investigateOption(command);
+        } else if (!investigate && trustTest) {
+            trustTestOption(command);
+        }
+
+    } else if (defaultMode && e.key == "ArrowUp" && commandHistory.length > 0) {
+        e.preventDefault();
+        // console.log(commandPosition)
+        if (commandPosition > 0) {
+            // console.log(commandHistory[commandPosition - 1])
+            terminalInput.innerHTML = commandHistory[commandPosition - 1]
+            placeCaretAtEnd(terminalInput);
+            commandPosition--;
+        } else {
+            // console.log(commandHistory[0])
+            terminalInput.innerHTML = commandHistory[0]
+            placeCaretAtEnd(terminalInput);
+            commandPosition = 0;
+        }
+    } else if (defaultMode && e.key == "ArrowDown" && commandHistory.length > 0) {
+        e.preventDefault();
+        // console.log(commandPosition)
+        if (commandPosition < commandHistory.length - 1) {
+            // console.log(commandHistory[commandPosition + 1])
+            terminalInput.innerHTML = commandHistory[commandPosition + 1]
+            placeCaretAtEnd(terminalInput);
+            commandPosition++;
+        } else {
+            // console.log(commandHistory[commandHistory.length - 1])
+            terminalInput.innerHTML = commandHistory[commandHistory.length - 1]
+            placeCaretAtEnd(terminalInput);
+            commandPosition = commandHistory.length - 1;
+        }
     }
 
 });
@@ -51,6 +105,8 @@ function runCommand(command) {
     instruction = command.split(" ")[0]
     variable = command.split(" ")[1]
 
+    
+
     // console.log(command.split(" "))
 
     // if (!(instruction in commands)) {
@@ -64,8 +120,14 @@ function runCommand(command) {
         lsFunction()
     } else if (instruction == "cd") {
         cdFunction(variable);
-    } else if (instruction == "help") {
+    } else if (instruction == "help" || instruction == "?") {
         helpFunction()
+    } else if (instruction == "hint" || instruction == "!") {
+        hintFunction()
+    } else if (instruction == "open") {
+        openFunction(variable)
+    // }  else if (instruction == "open") {
+    //     openFunction()
     } else {
         if (!(instruction in commands)) {
             output(["Command not found!"])
@@ -144,4 +206,147 @@ function helpFunction() {
     }
 
     output(helpInfo);
+}
+
+function hintFunction(text) {
+    if (text) {
+        console.log(text)
+    } else {
+        console.log("Generating hint")
+    }
+}
+
+function openFunction(file) {
+    posLocations = []
+    for (i=0; i<Object.keys(currentDirObject).length; i++) {
+        posLocations.push(Object.keys(currentDirObject)[i])
+    }
+    console.log("Opening file" + file)
+    if (files.includes(file) && posLocations.includes(file)) {
+        console.log("File true")
+
+        if (file == "fSociety.sh") {
+            openRootkit()
+        } else if (file == "passwords.txt") {
+            hintFunction("Nothing of much interst here, try looking or something suspicious.")
+        } else if (file == "README.md") {
+            hintFunction("You skim the file, something about hacking maybe mass government hacking? maybe try running the .sh file?")
+        }
+    } else {
+        output([file + " wasn't found or couldn't be opened, sure you spelt it right?"])
+    }
+}
+
+function placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
+}
+
+function openRootkit() {
+
+    defaultMode = false;
+
+    terminalOutputHolder.innerHTML = ""
+
+    var rootkitOutput = [
+        "fSociety Rootkit",
+        "<br>",
+        "{1}--Run Rootkit",
+        "{2}--Mount kernal and inspect code",
+        "{3}--Scan for viruses",
+        "<br>",
+        "{99}--Exit rootkit while its safe"
+    ]
+
+    output(rootkitOutput)
+
+}
+
+function rootkitOption(option) {
+    if (option == "1") {
+        gameOver(5)
+    } else if (option == "2") {
+        terminalOutputHolder.innerHTML = ""
+
+        var rootkitOutput = [
+            "fSociety Rootkit",
+            "<br>",
+            "Looking through the source code you become more suspicious.",
+            "<br>",
+            "{1}--Run",
+            "{2}--Report Code",
+            "{3}--Steal Code"
+        ]
+
+        output(rootkitOutput)
+        investigate = true;
+    } else if (option == "3") {
+        terminalOutputHolder.innerHTML = ""
+
+        var rootkitOutput = [
+            "fSociety Rootkit",
+            "<br>",
+            "Virus Scan was halted due to unexpected error",
+            "<br>",
+            "{1}--Trust",
+            "{2}--Don't Trust",
+            "{3}--Check out code for yourself"
+        ]
+
+        output(rootkitOutput)
+        trustTest = true;
+    } else if (option == "99") {
+        gameOver(1);
+    } else {
+        console.log("Error unknown input?")
+    }
+}
+
+function investigateOption(option) {
+    investigate = true;
+    if (option == "1") {
+        gameOver(5)
+    } else if (option == "2") {
+        gameOver(3)
+    } else if (option == "3") {
+        gameOver(2)
+    }
+}
+
+function trustTestOption(option) {
+    if (option == "1") {
+        openRootkit()
+        trustTest = false;
+    } else if (option == "2") {
+        gameOver(4)
+    } else if (option == "3") {
+        hintFunction("You don't trust the scan and decide to look at the code yourself.");
+        rootkitOption("2");
+        trustTest = false;
+    }
+}
+
+function gameOver(ending) {
+    var title = document.getElementById("endingTitle")
+    var description = document.getElementById("endingDescription")
+    var number = document.getElementById("endingNumber")
+
+    title.innerHTML = endings[ending].title
+    description.innerHTML = endings[ending].description
+    number.innerHTML = (ending + 1) + "/6"
+
+    document.getElementById("endingContainer").style.opacity = 1;
 }
